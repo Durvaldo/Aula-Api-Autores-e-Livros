55bc7cabc469ad85716d89e66c501aef59133135
Durvaldo Gonçalves Marques <durvaldomarques@gmail.com>
2025-08-20 13:36:51 -0300

    Update api.php

--- starter-kit-alunos/routes/api.php
+++ starter-kit-alunos/routes/api.php
@@ -2,40 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-/*
- * 🎯 TAREFA: Implementar as rotas abaixo
- * 
- * Você precisa:
- * 1. Importar os Controllers: AuthorController e BookController
- * 2. Definir as rotas resource para authors e books
- * 3. Adicionar rota especial para livros de um autor
- * 
- * Exemplo de implementação:
- * 
- * use App\Http\Controllers\AuthorController;
- * use App\Http\Controllers\BookController;
- * 
- * // Rotas de autores
- * Route::apiResource('authors', AuthorController::class);
- * 
- * // Rotas de livros  
- * Route::apiResource('books', BookController::class);
- * 
- * // Rota adicional: livros de um autor específico
- * Route::get('authors/{author}/books', [AuthorController::class, 'books']);
- */
+use App\Models\Author;
+use App\Models\Book;
 
 // TODO: Implementar suas rotas aqui
 
